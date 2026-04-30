b24d60b4ad322681476b9be8b97f21b8b478d56b
mattrias <mattrias60@gmail.com>
2026-04-30 14:07:40 +0800

    fix(helpMessage): changed description in help

--- events/helpMessage.js
+++ events/helpMessage.js
@@ -6,25 +6,32 @@ export async function execute(message) {
 
     try {
         const helpEmbed = new EmbedBuilder()
-            .setThumbnail('https://cdn.discordapp.com/attachments/1495990531589804034/1496773824354586664/SkrumBol_iskrabol_clean.png?ex=69eb1ac0&is=69e9c940&hm=d811fe3061656dda3a69153a112375da2ab660956d3a0118409572c4079d485f&')
             .setColor(0x3498DB) 
             .setTitle('🛠️ ScrumBot Help Guide')
             .setDescription('Welcome! Use this bot to manage daily scrum reports and track team progress.')
             .addFields(
                 { 
+                    name: '`/scrum`', 
+                    value: 'Shows a list of all existing projects and roles, and buttons to add and delete them.' 
+                },
+                { 
+                    name: '`/scrum-update`', 
+                    value: 'Shows updates, plans, and impediments of a selected project from a specified date.' 
+                },
+                { 
                     name: '`/add-report`', 
                     value: 'The main command. Select your **Project** and **Role** to open the daily update form.' 
                 },
                 { 
-                    name: '`/scrum-update`', 
-                    value: 'Shows a real-time list of updates for a selected project and date.' 
+                    name: '`/delete`', 
+                    value: 'Delete reports within a specified date range. Select the project and date range to clear reports from.' 
                 },
                 { 
                     name: '`!scrum-help`', 
                     value: 'Displays this helpful menu.' 
                 }
             )
-            .setFooter({ text: 'Marvin' })
+            .setFooter({ text: 'MarvTek' })
             .setTimestamp();
             
 
